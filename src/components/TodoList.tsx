4943cc97cff41f86d06d87188651ae267e617e08
Maxim Wilman <mwilman@brockhaus-ag.de>
2019-08-08 13:16:51 +0200

    remove item from the list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {AddTodoList} from "./AddTodoList";
+import {Button} from "./form-controls/Button";
 
 interface TodoItem {
     id: number;
@@ -7,19 +8,40 @@ interface TodoItem {
 }
 
 export const TodoList: React.FC = () => {
-    const [items, setItems] = React.useState([""]);
+    const [items, setItems] = React.useState([] as TodoItem[]);
 
     const addItem = (item: string) => {
         setItems([
             ...items,
-            item
+            {id: Math.random(), title: item}
         ]);
     };
+
+    const deleteElementInArray = (array: TodoItem[], element: TodoItem) => {
+        const index = array.findIndex(x => x.id === element.id);
+        if (index !== -1) {
+             return array.slice(0, index).concat(array.slice(index+1, array.length));
+        }
+        return array;
+    };
+
+    const deleteButton = (item: TodoItem) => {
+        setItems(deleteElementInArray(items, item))
+    };
+
+    function renderRow(x: TodoItem) {
+        return (
+            <div>
+                <p key={x.id}>{x.title}</p>
+                <Button name={"button_delete"} id={"button_delete"} label={"Loeschen!"} onClick={() => deleteButton(x)}/>
+            </div>
+        );
+    }
 
     return (
         <div>
             <AddTodoList handleSubmit={addItem}/>
-            {items.map(x => <p key={Math.random()}>{x}</p>)}
+            {items.map(renderRow)}
         </div>
     );
 };
